a10e43220bdeb558118183e2716dd4b9d32cc2a1
HM <hello@hazella.co>
2019-09-08 00:22:49 -0500

    Added custom toolbar

--- app/src/main/java/com/hm/lifeinspo/MainActivity.java
+++ app/src/main/java/com/hm/lifeinspo/MainActivity.java
@@ -1,19 +1,23 @@
 package com.hm.lifeinspo;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Main Quotes Display
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     /**
